cda3c6937d95e74a8d72e14bd3ea880549b6e1ef
Israel98-s <Kayodeisrael98@gmail.com>
2025-04-18 08:52:20 +0100

    Removed use_context parameter

--- bot.py
+++ bot.py
@@ -1,7 +1,6 @@
 from telegram import Update
-from telegram.ext import Updater, CommandHandler, MessageHandler, filters, CallbackContext  # Changed 'Filters' to 'filters'
+from telegram.ext import Updater, CommandHandler, MessageHandler, filters, CallbackContext
 
-# Replace 'YOUR_TOKEN' with the API token from BotFather
 TOKEN = "7838020402:AAFcpRVk-8Mk3Dmrye5WGdJMKj8OMJDGZug"
 
 def start(update: Update, context: CallbackContext):
@@ -11,11 +10,11 @@ def echo(update: Update, context: CallbackContext):
     update.message.reply_text(update.message.text)
 
 def main():
-    updater = Updater(TOKEN, use_context=True)
+    updater = Updater(TOKEN)  # Removed use_context=True
     dp = updater.dispatcher
 
     dp.add_handler(CommandHandler("start", start))
-    dp.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, echo))  # Updated this line too
+    dp.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, echo))
 
     updater.start_polling()
     updater.idle()
